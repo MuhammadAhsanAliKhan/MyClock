9c150f6f1539465a1c542e120bedcca0887003ba
MuhammadAhsanAliKhan <66951691+MuhammadAhsanAliKhan@users.noreply.github.com>
2022-08-20 21:15:09 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,30 +5,40 @@ import java.time.format.DateTimeFormatter;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.Timer;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
 /**
  *
  * @author Ahsan
  */
-public class Main {
+public class Main implements ActionListener {
     public Timer timer;
+    public Renderer rend;
     public static JFrame jframe;
     public static Main obj= new Main();
     public final static int WIDTH=400, HEIGHT=100;
     
     public static void main(String[] args) {
-        obj.window();
+        obj= new Main();
     }
-    public void window() {
+    public Main() {
         jframe= new JFrame("MyClock");
         
-        JLabel jlabel= new JLabel(obj.time());
-        
+        JLabel jlabel= new JLabel(time());
+        rend= new Renderer();
+        timer= new Timer(10, this);
+        jframe.add(rend);
         jframe.getContentPane().add(jlabel);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	jframe.setSize(WIDTH, HEIGHT);
 	jframe.setResizable(false);
         jframe.setLocationRelativeTo(null);
         jframe.setVisible(true);
+        
+        timer.start();
     }
     public String time() {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
@@ -36,4 +46,29 @@ public class Main {
         String time_var= dtf.format(now);
         return time_var;  
     }
+    
+    public void repaint(Graphics g) 
+    {
+            //paint background
+            g.setColor(Color.yellow);
+            g.fillRect(0, 0, WIDTH, HEIGHT);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        rend.repaint();
+    }
+}
+
+class Renderer extends JPanel
+	{
+
+		private static final long serialVersionUID = 1L;
+		
+		@Override
+		public void paint(Graphics g) 
+		{
+			super.paintComponents(g);
+			Main.obj.repaint(g);
+		}
 }
